4b016fd3b074707587e0524fb3cdd3725bc7eb57
eric2121 <e_casault@hotmail.com>
2014-08-29 14:54:17 -0400

    Commit initial

---
+++ TP_1/Controllers/ContactController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TP_1.Models;
+
+namespace TP_1.Controllers
+{
+    public class ContactController : Controller
+    {
+
+        [HttpGet]
+        public ActionResult ContactForm()
+        {
+            return View();
+
+        }
+
+        [HttpPost]
+        public ActionResult ContactForm(ContactModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                InsertContact(model.Name, model.Email, model.Comments);
+                TempData["notice"] = "Your form was submitted.";
+                return RedirectToAction("Index", "Home");
+            }
+            return View();
+        }
+
+        private void InsertContact(string name, string email, string comments)
+        {
+
+        }
+
+    }
+}
---
+++ TP_1/Models/ContactModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace TP_1.Models
+{
+    public class ContactModel
+    {
+        [Required(ErrorMessage="Required")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Required")]
+        [RegularExpression(@".*@.*", ErrorMessage="Must be a valid email")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Required")]
+        public string Comments { get; set; }
+    }
+}
