c156820e7c76347fd469564740ee4d163a6cf253
eric2121 <e_casault@hotmail.com>
2014-08-29 16:17:19 -0400

    tp2

---
+++ TP1/Controllers/HelloWorldController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TP1.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        //
+        // GET: /HelloWorld/
+
+       /* public ActionResult Index()
+        {
+            //return View();
+            return 
+        }*/
+        public string Index()
+        {
+            return "This is my <b>default</b> action...";
+        }
+
+        //
+        // GET: /HelloWorld/Welcome/
+
+        public string Welcome(string name, int numTimes = 1)
+        {
+            return HttpUtility.HtmlEncode("Hello " + name + ", NumTimes is: " + numTimes);
+        }
+
+    }
+}
